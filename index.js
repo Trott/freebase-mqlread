b1b6c3480ba14cff9c710ab1708e1fb5081748e3
Rich Trott <rtrott@gmail.com>
2014-11-24 21:38:09 -0800

    switch from request-json to hyperequest so we can use browserify, closes #1

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-var client = require('request-json').newClient('https://www.googleapis.com');
+var hyperquest = require('hyperquest');
 var querystring = require('querystring');
 
 exports.mqlread = function(query, options, callback) {
-  var url = '/freebase/v1/mqlread?' +
+  var url = 'https://www.googleapis.com/freebase/v1/mqlread?' +
     querystring.stringify({query: query}) +
     '&' + querystring.stringify(options);
-  client.get(url, function(err, res, data) {
-    if (!err) {
-    	if (res.statusCode !== 200) {
-    		err = new Error('Received status code ' + res.statusCode);
-    	}
-    	if (data && data.error) {
-      	err = new Error(data.error.message);
-    	}
+
+  hyperquest(url, {}, function(err, res) {
+    if (err) {
+      callback(err);
+      return;
     }
-    callback(err, data);
+    var body = '';
+    if (res.statusCode !== 200) {
+      err = new Error('Received status code ' + res.statusCode);
+      callback(err);
+      return;
+    }
+    res.on('data', function (chunk) {
+      body +=chunk;
+    });
+
+    res.on('end', function () {
+      var data;
+      try {
+        data = JSON.parse(body);
+      } catch (e) {
+        callback(new Error('Error in received JSON:' + e.message));
+        return;
+      }
+      if (data.error) {
+       err = new Error(data.error.message);
+     }
+      callback(err, data);
+    });
   });
 };
